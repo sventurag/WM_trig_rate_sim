99c08671942ae11406ef15ae67a37eaed4c0f6f3
jdduron <40073656+jdduron@users.noreply.github.com>
2019-09-03 11:04:33 -1000

    - changed plots, added histogram, calculates frequency

--- testMain.py
+++ testMain.py
@@ -9,8 +9,8 @@ darkProbPerPmtPerTick = clockTick_ns/darkPeriod_ns
 numPmtsPerAsic = 4
 
 digitizeAndReadoutTime_ns = 24000
-numWindows = 512
-window_buffer=np.zeros((512,), dtype=np.int)
+numWindows = 255
+window_buffer=np.zeros((numWindows,), dtype=np.int)
 
 timeNow_ns = 0
 stopTime_ns = 1e9
@@ -25,13 +25,15 @@ buffer_index_head = 0
 buffer_index_tail = 0
 buffer_digit_time = 0
 queue_count = 0
+event_count = 0
 flag=0
 
-plt.subplot(211)
-plt.stem(window_buffer)
-plt.subplot(212)
-plt.plot(times, lengths)
-plt.show()
+print("start sim!")
+#plt.subplot(211)
+#plt.stem(window_buffer)
+#plt.subplot(212)
+#plt.plot(times, lengths)
+#plt.show()
 
 while timeNow_ns < stopTime_ns and len(queue) < numWindows:
     if len(queue) > 0:
@@ -43,6 +45,7 @@ while timeNow_ns < stopTime_ns and len(queue) < numWindows:
     if random() < darkProbPerPmtPerTick*numPmtsPerAsic:
         queue.append(digitizeAndReadoutTime_ns)
         queue_count +=1
+        event_count +=1
         
         if flag ==0 :
             buffer_digit_time = digitizeAndReadoutTime_ns
@@ -51,7 +54,7 @@ while timeNow_ns < stopTime_ns and len(queue) < numWindows:
         if window_buffer[buffer_index_head] == 2:
             
             print("buffer full")
-            print("time now:")
+            print("time now (ns):")
             print(timeNow_ns)
             print("buffer head:")
             print(buffer_index_head)
@@ -68,7 +71,7 @@ while timeNow_ns < stopTime_ns and len(queue) < numWindows:
             plt.show()
             break
 
-        if buffer_index_head < 511:
+        if buffer_index_head < numWindows-1:
             buffer_index_head +=1
         else:
             buffer_index_head=0
@@ -78,19 +81,22 @@ while timeNow_ns < stopTime_ns and len(queue) < numWindows:
         lengths.append(len(queue))
     lastLength = len(queue)
     if step%1000000 == 0:
-        print("time now:")
-        print(timeNow_ns)
-        print("buffer head:")
-        print(buffer_index_head)
-        print("buffer tail:")
-        print(buffer_index_tail)
+        #print("time now (ns):")
+        #print(timeNow_ns)
+        #print("buffer head:")
+        #print(buffer_index_head)
+        #print("buffer tail:")
+        #print(buffer_index_tail)
 
-        plt.close()
-        plt.subplot(211)
-        plt.stem(window_buffer)
-        plt.subplot(212)
-        plt.plot(times, lengths)
-        plt.show()
+        #plt.close()
+        #plt.subplot(211)
+        #plt.stem(window_buffer)
+        #plt.subplot(212)
+        #plt.plot(times, lengths)
+        #plt.show()
+
+        print("run %")
+        print(float(timeNow_ns)/stopTime_ns*100)
             
     timeNow_ns += clockTick_ns
     if queue_count > 0 :
@@ -99,7 +105,7 @@ while timeNow_ns < stopTime_ns and len(queue) < numWindows:
         if buffer_digit_time < 0 :
             window_buffer[buffer_index_tail]-=1
             queue_count -=1
-            if buffer_index_tail < 511:
+            if buffer_index_tail < numWindows-1:
                 buffer_index_tail+=1
             else :
                 buffer_index_tail=0
@@ -111,16 +117,29 @@ while timeNow_ns < stopTime_ns and len(queue) < numWindows:
     step += 1
 
 print("Sim End")
-print("time now:")
+print("time now (ns):")
 print(timeNow_ns)
 print("buffer head:")
 print(buffer_index_head)
 print("buffer tail:")
 print(buffer_index_tail)
+print("Total Event count:")
+print(event_count)
+event_frequency =event_count/stopTime_ns*1e9
+print("Event Rate (Hz):")
+print(event_frequency)
 
 plt.clf
 plt.subplot(211)
-plt.stem(window_buffer)
-plt.subplot(212)
 plt.plot(times, lengths)
+plt.xlabel('time (ns)')
+plt.ylabel('% utilization')
+plt.title('Memory Buffer Utilization over Time due to 4 PMT 10kHz noise')
+
+
+plt.subplot(212)
+plt.hist(lengths,25)
+plt.xlabel('% utilization')
+plt.ylabel('Frequency')
+plt.title('Frequency of % Utilization')
 plt.show()
